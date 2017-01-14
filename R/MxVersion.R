9dc1a3cc3b1dce658cdf1175eda1d49fc82cf56f
Joshua N. Pritikin <jpritikin@pobox.com>
2017-01-14 11:12:30 +0000

    version 2.7.3

--- R/MxVersion.R
+++ R/MxVersion.R
@@ -13,7 +13,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-gitVersion <- "v2.7.2"
+gitVersion <- "v2.7.3"
 
 mxVersion <- function (model=NULL, verbose=TRUE) {
     pvers <- try(packageVersion("OpenMx"))

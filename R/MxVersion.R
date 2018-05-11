e5191bcd689c98fda27c80fce12d34e321600e54
Joshua N. Pritikin <jpritikin@pobox.com>
2018-05-11 19:29:23 +0000

    version 2.9.9

--- R/MxVersion.R
+++ R/MxVersion.R
@@ -1,5 +1,5 @@
 #
-#   Copyright 2007-2015 The OpenMx Project
+#   Copyright 2007-2018 by the individuals mentioned in the source code history
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-gitVersion <- "v2.9.6"
+gitVersion <- "v2.9.9"
 
 mxVersion <- function (model=NULL, verbose=TRUE) {
 	pvers <- pkg_globals$myVersion

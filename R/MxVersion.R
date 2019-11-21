e1ec239c25751e37b8f5d569d2d09244ae923fab
Joshua N. Pritikin <jpritikin@pobox.com>
2019-11-21 04:30:06 +0000

    version 2.15.4

--- R/MxVersion.R
+++ R/MxVersion.R
@@ -13,7 +13,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-gitVersion <- "v2.15.3"
+gitVersion <- "v2.15.4"
 
 mxVersion <- function (model=NULL, verbose=TRUE) {
 	pvers <- pkg_globals$myVersion

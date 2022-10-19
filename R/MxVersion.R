ebf73b069fc1d5b17abb7d6193dd3b670b74bff3
Joshua N. Pritikin <jpritikin@pobox.com>
2022-10-19 09:38:03 +0000

    version 2.20.7

--- R/MxVersion.R
+++ R/MxVersion.R
@@ -13,7 +13,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-gitVersion <- "v2.20.6-dirty"
+gitVersion <- "v2.20.7"
 
 mxVersion <- function (model=NULL, verbose=TRUE) {
 	pvers <- pkg_globals$myVersion
